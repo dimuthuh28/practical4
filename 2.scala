0a217ba7a6ec5ce9ba852f314ba7538df5159ce7
Dimuthu Harshamal <78658133+dimuthuh28@users.noreply.github.com>
2024-07-10 19:57:53 +0530

    Update 2.scala

--- 2.scala
+++ 2.scala
@@ -1,15 +1,17 @@
-object secondapp {
+import scala.io.StdIn.readLine
 
-  def PatternMatching(number: Int): Unit = number match {
-    case x if x <= 0     => println("The number is Negative or zero")
-    case x if x % 2 == 0 => println("The number is even")
-    case _x => println("The number is odd")
+object patternMatching extends App{
+
+  print("Enter a Integer: ")
+  val intg: Int = readLine().toInt
+
+  def pm(intg: Int): Unit = intg <= 0 match{
+    case true  => println("Negetive/Zero")
+    case false => intg % 2 match{
+      case 0 => println("Even number is given.")
+      case 1 => println("Odd number is given ")
+    }
   }
 
-  def main(args: Array[String]): Unit = {
-    print("Enter a number: ")
-    var number = scala.io.StdIn.readInt()
-
-    PatternMatching(number)
-  }
+  pm(intg)
 }
